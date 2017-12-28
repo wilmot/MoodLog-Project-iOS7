10ce8bc45d11c1ea8fe545d43d356a1426159de8
Barry Langdon-Lassagne <wilmot@me.com>
2017-12-28 13:27:22 -0800

    Demo records for screen shots and more realistic testing

--- MoodLog Tests/MoodLog_Tests.m
+++ MoodLog Tests/MoodLog_Tests.m
@@ -59,6 +59,77 @@
     return (int)from + arc4random() % (to-from+1);
 }
 
+- (void)testCreateDemoRecords {
+    UIApplication *sharedApplication = [UIApplication sharedApplication];
+    MlAppDelegate *delegate =(MlAppDelegate *)sharedApplication.delegate;
+    MoodLogEvents *newMoodLogEntry;
+    NSArray *emotionArray;
+    MlMoodDataItem *aMood;
+    emotionArray = [delegate.emotionsFromPList copy];
+    NSArray *entries = @[
+                         @[@"-10", @"Expected to hear back from the college by today. I'm impatient! Also bored. Also tired, I guess. I'm not exactly sure how I feel today. I think I need something to distract me.", @[@"Bored", @"Disappointed", @"Irritated"], @[@-3,@2,@-10,@1,@5,@-6]],
+                         @[@"-9", @"Windy outside. I don't want to do my chores. Want to stay on the couch and read a book! Hole up here where it's safe and warm until the world comes to find me.", @[@"Cautious", @"Uneasy"], @[@-4,@-2,@-10,@-1,@4,@-5]],
+                         @[@"-8", @"Sis's soccer team won! They played their rivals and this was the first time in three years that they won. 3-2. Sis scored one of the goals even! Luckily yesterday's wind died down and it was warm and sunny.", @[@"Proud"], @[@9,@5,@9,@6,@8,@2]],
+                         @[@"-7", @"I've been accepted! I can't believe it. I'm going to college! There's so much to do. This is really huge. Can't write, gotta call Samantha.", @[@"Ecstatic", @"Happy", @"Triumphant"], @[@10,@6,@10,@-5,@7,@5]],
+                         @[@"-6", @"There's this lady that walks her dog in our neighborhood. I swear she passes our apartments at the exact same time every day. I don't want my life to be like that, that predictable. I'm going to follow the path of least expectance.", @[@"Aggravated", @"Displeased", @"Suspicious"], @[@-3,@-5,@8,@5,@3,@5]],
+                         @[@"-5", @"The bus driver was really funny today. I was sitting in front and he told stories about his time as a grill cook and the things that they would do as practical jokes to each other at the restaurant. Bus was mostly empty, so just me and one other passenger heard the stories.", @[@"Amused"], @[@8,@10,@4,@5,@9,@8]],
+                         @[@"-5", @"Best friend is sad. They haven't heard anything. They're afraid that maybe they didn't get in and that's why they've heard nothing. I feel bad for Sam. I hope they're wrong.", @[@"Caring", @"Embarrassed", @"Regretful", @"sad", @"Sympathetic"], @[@-8,@-4,@-6,@4,@5,@4]],
+                         @[@"-4", @"My best friend is angry with me. Says it's not fair. Says I'll lose touch with them. It wasn't exactly a fight, but we aren't speaking at the moment. Also the bus was late, but I made it to work just in time.", @[@"Angry", @"Annoyed", @"Irritated", @"Resenting"], @[@-10,@-10,@7,@-10,@5,@-5]],
+                         @[@"-3", @"I'm feeling down today, thinking about leaving home and living in a strange place and not seeing my sister. She's sick today too, and I'm afraid I'm going to come down sick. Also, it's raining. Bleah. And the college is kinda weird - no grades, funky schedule. I don't know if I'll fit in.", @[@"Fearful", @"Insecure", @"Uneasy", @"Worried"], @[@-10,@-6,@-10,@-4,@-7,@-4]],
+                         @[@"-2", @"Today is my official last day at my job. I'm glad it's over, but I also will miss my coworkers. Some of it was pretty fun.", @[@"Melancholy", @"Pleased"], @[@4,@4,@5,@6,@3,@5]],
+                         @[@"-2", @"My boss wrote me a recommendation! I'll be able to take this with me and use it to help get a job sometime in the future.", @[@"Glad", @"Proud"], @[@8,@6,@4,@-3,@6,@8]],
+                         @[@"-1", @"Mom was really empathetic today. She and I had a long talk about college and I told her my fears and then we went out for ice cream and to look at college supplies. It helped. She calls the college 'non-traditional' which is mom-speak for 'funky.' But she thinks they'll be really good for me, a good fit. Hope so.", @[@"Empathetic", @"Relieved", @"Sentimental"], @[@7,@3,@3,@10,@7,@7]],
+                         @[@"-0", @"Everything just changed — they called to tell me they got in! They're accepted! We'll be going to college together! Apparently I’m totally forgiven too, so we’re best friends again. Tomorrow they're coming over. We have a lot of planning to do.", @[@"Elated", @"Relieved"], @[@10,@4,@10,@8,@7,@5]]
+                         ];
+    for (NSArray *entry in entries) {
+        newMoodLogEntry = [[delegate masterViewController] insertNewObjectAndReturnReference:self];
+        NSDate *theDate = [NSDate date];
+        int backdate = ((NSString *)entry[0]).intValue;
+        theDate = [theDate dateByAddingTimeInterval:(86400*backdate)];
+        theDate = [theDate dateByAddingTimeInterval:[self randomNumberFrom:-(24*60*60)/6 to:(24*60*60)/24]];
+        NSCalendar *calendar = [NSCalendar currentCalendar];
+        NSDateComponents *components;
+
+        NSString *journal = (NSString *)entry[1];
+        newMoodLogEntry.dateCreated = theDate;
+        newMoodLogEntry.date = theDate;
+        components = [calendar components:(NSCalendarUnitYear | NSCalendarUnitMonth) fromDate:newMoodLogEntry.date];
+        newMoodLogEntry.header = [NSString stringWithFormat:@"%ld", (long)([components year] * 1000) + [components month]];
+        newMoodLogEntry.journalEntry = journal;
+
+        NSArray *moods = (NSArray *)entry[2];
+        NSArray *factors = (NSArray *)entry[3];
+        NSString *test = @"";
+        for (NSString *mood in moods) {
+            test = [test stringByAppendingFormat:@"%@ ", mood];
+        }
+        newMoodLogEntry.overall = (NSNumber *)(factors[0]);
+        newMoodLogEntry.stress = (NSNumber *)(factors[1]);
+        newMoodLogEntry.energy = (NSNumber *)(factors[2]);
+        newMoodLogEntry.thoughts = (NSNumber *)(factors[3]);
+        newMoodLogEntry.health = (NSNumber *)(factors[4]);
+        newMoodLogEntry.sleep = (NSNumber *)(factors[5]);
+        for (NSString *mood in moods) {
+            for (MlMoodDataItem *emotion in emotionArray) {
+                if ([emotion.mood.lowercaseString isEqualToString:mood.lowercaseString]) {
+                    aMood = emotion;
+                    [aMood setValue:[NSNumber numberWithBool:YES] forKey:@"selected"];
+                    // Add emotion to the record
+                    Emotions *emotion = [NSEntityDescription insertNewObjectForEntityForName:@"Emotions" inManagedObjectContext:[delegate managedObjectContext]];
+                    emotion.name = aMood.mood;
+                    emotion.category = aMood.category;
+                    emotion.parrotLevel = [NSNumber numberWithInt:(int)[aMood.parrotLevel integerValue]];
+                    emotion.feelValue = [NSNumber numberWithInt:(int)[aMood.feelValue integerValue]];
+                    emotion.facePath = aMood.facePath;
+                    emotion.selected = [NSNumber numberWithBool:YES];
+                    emotion.logParent = newMoodLogEntry; // current record
+                }
+            }
+        }
+    }
+    [delegate saveContext];
+}
+
 - (void)testCreatingAFewRecords {
     UIApplication *sharedApplication = [UIApplication sharedApplication];
     MlAppDelegate *delegate =(MlAppDelegate *)sharedApplication.delegate;
@@ -303,6 +374,7 @@
                 [delegate saveContext];
             }
         }
+        [delegate saveContext];
     }
     
     NSLog(@"Finishing testDeletingAllRecords.");
